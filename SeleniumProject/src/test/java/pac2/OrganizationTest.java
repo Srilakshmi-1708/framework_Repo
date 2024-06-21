03d0f1f12d66b6141967a6750f71afb5580ec564
Srilakshmi-1708 <srilakshmirajalbandi@gmail.com>
2024-06-21 13:05:57 +0530

    First Push

---
+++ SeleniumProject/src/test/java/pac2/OrganizationTest.java
@@ -0,0 +1,17 @@
+package pac2;
+
+import org.testng.annotations.Test;
+
+public class OrganizationTest {
+	
+	@Test
+	public void createOrgTest() {
+		System.out.println("execute createOrgTest ");
+	}
+
+	@Test
+	public void modifyOrgTest() {
+		System.out.println("execute modifyOrgTest ");
+	}
+
+}
